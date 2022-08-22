dacd2261d33eaef3d6e6d2d1e59814c17aa372f1
chai2010 <chaishushan@gmail.com>
2022-08-22 23:58:00 +0800

    删除 wabt

--- internal/3rdparty/wabt/internal/wabt-1.0.29/config.h
+++
@@ -1,6 +0,0 @@
-// 版权 @2022 凹语言 作者。保留所有权利。
-
-#include "./config_darwin_amd64.h"
-
-// TODO: windows
-// TODO: linux
--- internal/3rdparty/wabt/zz_leb128.cc
+++
@@ -1,8 +0,0 @@
-// 版权 @2022 凹语言 作者。保留所有权利。
-
-// Auto generated, DONOT EDIT!!!
-
-//go:build cgo
-// +build cgo
-
-#include "internal/wabt-1.0.29/src/leb128.cc"
