40a764cebf55ba94408dfbf798043e8acd569062
chai2010 <chaishushan@gmail.com>
2022-09-07 21:37:33 +0800

    wat: 补充版本信息

--- internal/backends/compiler_wat/wir/wat/module.go
+++ internal/backends/compiler_wat/wir/wat/module.go
@@ -1,4 +1,15 @@
+// 版权 @2022 凹语言 作者。保留所有权利。
+
 package wat
+
+// 模块对象
+type Module struct {
+	Name    string
+	Imports []Import
+	Funcs   []*Function
+
+	BaseWat string
+}
 
 func (m *Module) String() string {
 	s := "(module $__walang__\n"
