edb8534244aae120f0d32fec510196f7a55825cf
Ben Shi <powerman1st@163.com>
2022-11-05 18:56:35 +0800

    llvm: fix a bug in printing u16/i16/u8/i8/f32 values.

--- internal/backends/compiler_llvm/compile_value.go
+++ internal/backends/compiler_llvm/compile_value.go
@@ -5,6 +5,7 @@ package compiler_llvm
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 	"strconv"
 
 	"github.com/wa-lang/wa/internal/ssa"
@@ -357,6 +358,11 @@ func (p *Compiler) compileBuiltin(val *ssa.CallCommon) error {
 	return nil
 }
 
+type printArg struct {
+	name string
+	typ  string
+}
+
 func (p *Compiler) compilePrint(val *ssa.CallCommon, ln bool) error {
 	index := len(p.fmts)
 	size := int(0)
@@ -364,6 +370,9 @@ func (p *Compiler) compilePrint(val *ssa.CallCommon, ln bool) error {
 
 	// Formulate the format string.
 	for _, arg := range val.Args {
+		if _, ok := arg.Type().(*types.Basic); !ok {
+			return errors.New("can only print scalar typed values")
+		}
 		f, s := getValueFmt(arg, p.target)
 		format += f
 		size += s
@@ -377,18 +386,56 @@ func (p *Compiler) compilePrint(val *ssa.CallCommon, ln bool) error {
 	format += "\\00"
 	size += 1
 
+	// Emit some type promote instructions for f32/i16/i8/u16/u8 parameters.
+	varMap := map[int]printArg{}
+	for i, arg := range val.Args {
+		if t, ok := arg.Type().(*types.Basic); ok {
+			instr, ty := "", ""
+			switch t.Kind() {
+			case types.Float32:
+				instr, ty = "fpext", "double"
+			case types.Uint16, types.Uint8:
+				instr, ty = "zext", "i32"
+			case types.Int16, types.Int8:
+				instr, ty = "sext", "i32"
+			default:
+			}
+			if len(instr) > 0 {
+				name := fmt.Sprintf("%%ptmp%d", rand.Int())
+				varMap[i] = printArg{name, ty}
+				p.output.WriteString("  ")
+				p.output.WriteString(name)
+				p.output.WriteString(" = ")
+				p.output.WriteString(instr)
+				p.output.WriteString(" ")
+				p.output.WriteString(getTypeStr(arg.Type(), p.target))
+				p.output.WriteString(" ")
+				p.output.WriteString(getValueStr(arg))
+				p.output.WriteString(" to ")
+				p.output.WriteString(ty)
+				p.output.WriteString("\n")
+			}
+		}
+	}
+
 	// Emit the call instruction and the first parameter.
 	p.output.WriteString("  call i32 (i8*, ...) @printf(i8* getelementptr inbounds (")
 	p.output.WriteString(fmt.Sprintf("[%d x i8], [%d x i8]* @printfmt%d, i16 0, i16 0)", size, size, index))
 
 	// Emit other parameters and finish the call instruction.
-	for _, arg := range val.Args {
+	for i, arg := range val.Args {
 		// Omit constant strings.
 		if !isConstString(arg) {
 			p.output.WriteString(", ")
-			p.output.WriteString(getTypeStr(arg.Type(), p.target))
-			p.output.WriteString(" ")
-			p.output.WriteString(getValueStr(arg))
+			if vn, ok := varMap[i]; ok {
+				p.output.WriteString(vn.typ)
+				p.output.WriteString(" ")
+				p.output.WriteString(vn.name)
+			} else {
+				p.output.WriteString(getTypeStr(arg.Type(), p.target))
+				p.output.WriteString(" ")
+				p.output.WriteString(getValueStr(arg))
+			}
 		}
 	}
 	p.output.WriteString(")\n")
