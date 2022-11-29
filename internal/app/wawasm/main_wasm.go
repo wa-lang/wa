f6a14f39bedeeea8b4b81f0bdd2b2dd3d5bbc68e
xxx <humengmingxx@gmail.com>
2022-11-29 23:26:11 +0800

    feat：adjust code and export fmt

--- internal/app/wawasm/main_wasm.go
+++ internal/app/wawasm/main_wasm.go
@@ -8,14 +8,28 @@ import (
 	"wa-lang.org/wa/api"
 )
 
+func waGenerateWat(code string) string {
+	wat, err := api.BuildFile(api.DefaultConfig(), "hello.wa", code)
+	if err != nil {
+		return err.Error()
+	} else {
+		return string(wat)
+	}
+}
+
+func waFormatCode(code string) string {
+	code, err := api.FormatCode("hello.wa", code)
+	if err != nil {
+		return err.Error()
+	} else {
+		return code
+	}
+}
+
 func main() {
 	window := js.Global().Get("window")
-	waCode := window.Get("waCode").String()
+	waCode := window.Get("__WA_CODE__").String()
 
-	wat, err := api.BuildFile(api.DefaultConfig(), "hello.wa", waCode)
-	if err != nil {
-		window.Set("waWat", err.Error())
-	} else {
-		window.Set("waWat", string(wat))
-	}
+	window.Set("__WA_WAT__", waGenerateWat(waCode))
+	window.Set("__WA_FMT_CODE__", waFormatCode(waCode))
 }
