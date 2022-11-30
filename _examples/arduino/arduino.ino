3e1ba7bee53cadf661c11f957ab9e4c3a3c3e9bf
chai2010 <chaishushan@gmail.com>
2022-11-30 21:51:00 +0800

    arduino 添加 wa 例子, 可在 nano 33 开发板执行

---
+++ _examples/arduino/arduino.ino
@@ -0,0 +1,209 @@
+/*
+ * Wasm3 - high performance WebAssembly interpreter written in C.
+ * Copyright © 2020 Volodymyr Shymanskyy, Steven Massey.
+ * All rights reserved.
+ */
+
+#include <wasm3.h>
+#include <m3_env.h>
+
+/*
+ * Configuration
+ */
+
+// Redefine the default LED pin here, if needed
+#ifndef LED_PIN
+#define LED_PIN             LED_BUILTIN
+#endif
+
+#define WASM_STACK_SLOTS    1024
+#define NATIVE_STACK_SIZE   (32*1024)
+
+// For (most) devices that cannot allocate a 64KiB wasm page
+#define WASM_MEMORY_LIMIT   8096
+
+/*
+ * WebAssembly app
+ *
+ * This is essentially a simple "Blink" sketch, compiled to WebAssembly
+ * You can build a wasm binary using C/C++, Rust, AssemblyScript, TinyGo, ...
+ * See https://github.com/wasm3/wasm3-arduino/tree/master/wasm_apps for details
+ */
+
+#include "app.wasm.h"
+
+/*
+ * API bindings
+ *
+ * Note: each RawFunction should complete with one of these calls:
+ *   m3ApiReturn(val)   - Returns a value
+ *   m3ApiSuccess()     - Returns void (and no traps)
+ *   m3ApiTrap(trap)    - Returns a trap
+ */
+
+m3ApiRawFunction(m3_arduino_millis)
+{
+    m3ApiReturnType (uint32_t)
+
+    m3ApiReturn(millis());
+}
+
+m3ApiRawFunction(m3_arduino_delay)
+{
+    m3ApiGetArg     (uint32_t, ms)
+
+    // You can also trace API calls
+    //Serial.print("api: delay "); Serial.println(ms);
+
+    delay(ms);
+
+    m3ApiSuccess();
+}
+
+// This maps pin modes from arduino_wasm_api.h
+// to actual platform-specific values
+uint8_t mapPinMode(uint8_t mode)
+{
+    switch(mode) {
+    case 0: return INPUT;
+    case 1: return OUTPUT;
+    case 2: return INPUT_PULLUP;
+    }
+    return INPUT;
+}
+
+m3ApiRawFunction(m3_arduino_pinMode)
+{
+    m3ApiGetArg     (uint32_t, pin)
+    m3ApiGetArg     (uint32_t, mode)
+
+#if !defined(PARTICLE)
+    typedef uint8_t PinMode;
+#endif
+    pinMode(pin, (PinMode)mapPinMode(mode));
+
+    m3ApiSuccess();
+}
+
+m3ApiRawFunction(m3_arduino_digitalWrite)
+{
+    m3ApiGetArg     (uint32_t, pin)
+    m3ApiGetArg     (uint32_t, value)
+
+    digitalWrite(pin, value);
+
+    m3ApiSuccess();
+}
+
+m3ApiRawFunction(m3_arduino_getPinLED)
+{
+    m3ApiReturnType (uint32_t)
+
+    m3ApiReturn(LED_PIN);
+}
+
+m3ApiRawFunction(m3_arduino_print)
+{
+    m3ApiGetArgMem  (const uint8_t *, buf)
+    m3ApiGetArg     (uint32_t,        len)
+
+    Serial.write(buf, len);
+    m3ApiSuccess();
+}
+
+M3Result  LinkArduino  (IM3Runtime runtime)
+{
+    IM3Module module = runtime->modules;
+    const char* arduino = "arduino";
+
+    m3_LinkRawFunction (module, arduino, "millis",           "i()",    &m3_arduino_millis);
+    m3_LinkRawFunction (module, arduino, "delay",            "v(i)",   &m3_arduino_delay);
+    m3_LinkRawFunction (module, arduino, "pinMode",          "v(ii)",  &m3_arduino_pinMode);
+    m3_LinkRawFunction (module, arduino, "digitalWrite",     "v(ii)",  &m3_arduino_digitalWrite);
+
+    // Test functions
+    m3_LinkRawFunction (module, arduino, "getPinLED",        "i()",    &m3_arduino_getPinLED);
+    m3_LinkRawFunction (module, arduino, "print",            "v(*i)",  &m3_arduino_print);
+
+    return m3Err_none;
+}
+
+/*
+ * Engine start, liftoff!
+ */
+
+#define FATAL(func, msg) { Serial.print("Fatal: " func " "); Serial.println(msg); return; }
+
+void wasm_task(void*)
+{
+    M3Result result = m3Err_none;
+
+    IM3Environment env = m3_NewEnvironment ();
+    if (!env) FATAL("NewEnvironment", "failed");
+
+    IM3Runtime runtime = m3_NewRuntime (env, WASM_STACK_SLOTS, NULL);
+    if (!runtime) FATAL("NewRuntime", "failed");
+
+#ifdef WASM_MEMORY_LIMIT
+    runtime->memoryLimit = WASM_MEMORY_LIMIT;
+#endif
+
+    IM3Module module;
+    result = m3_ParseModule (env, &module, app_wasm, app_wasm_len);
+    if (result) FATAL("ParseModule", result);
+
+    result = m3_LoadModule (runtime, module);
+    if (result) FATAL("LoadModule", result);
+
+    result = LinkArduino (runtime);
+    if (result) FATAL("LinkArduino", result);
+
+    IM3Function f;
+    result = m3_FindFunction (&f, runtime, "_start");
+    if (result) FATAL("FindFunction", result);
+
+    Serial.println("Running WebAssembly...");
+    result = m3_CallV (f);
+
+    // Should not arrive here
+
+    if (result) {
+        M3ErrorInfo info;
+        m3_GetErrorInfo (runtime, &info);
+        Serial.print("Error44: ");
+        Serial.print(result);
+        Serial.print(" (");
+        Serial.print(info.message);
+        Serial.println(")");
+        if (info.file && strlen(info.file) && info.line) {
+            Serial.print("At ");
+            Serial.print(info.file);
+            Serial.print(":");
+            Serial.println(info.line);
+        }
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(100);
+
+    // Wait for serial port to connect
+    // Needed for native USB port only
+    while(!Serial) {}
+
+    Serial.println("\nWasm3 v" M3_VERSION " (" M3_ARCH "), build " __DATE__ " " __TIME__);
+
+#ifdef ESP32
+    // On ESP32, we can launch in a separate thread
+    xTaskCreate(&wasm_task, "wasm3", NATIVE_STACK_SIZE, NULL, 5, NULL);
+#else
+    wasm_task(NULL);
+#endif
+}
+
+void loop()
+{
+    delay(100);
+}
