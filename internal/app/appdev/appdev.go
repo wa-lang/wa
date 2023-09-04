e1b61af9af078daca55ad5b9378bccfbdbddea89
chai2010 <chaishushan@gmail.com>
2023-09-04 23:57:09 +0800

    实现简版 wa doc 子命令

--- internal/app/appdev/appdev.go
+++ internal/app/appdev/appdev.go
@@ -1,8 +1,13 @@
 package appdev
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"wa-lang.org/wa/api"
 	"wa-lang.org/wa/internal/3rdparty/cli"
@@ -13,19 +18,96 @@ var CmdDev = &cli.Command{
 	Hidden: true,
 	Name:   "debug",
 	Usage:  "only for dev/debug",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "hello",
+		},
+		&cli.BoolFlag{
+			Name:  "count-code-lines",
+		},
+	},
 	Action: func(c *cli.Context) error {
-		wat, err := api.BuildFile(
-			config.DefaultConfig(),
-			"hello.wa", "func main() { println(123) }",
-		)
-		if err != nil {
-			if len(wat) != 0 {
-				fmt.Println(string(wat))
-			}
-			fmt.Println(err)
-			os.Exit(1)
+		if c.Bool("count-code-lines") {
+			RunCountCodeLines()
+			os.Exit(0)
 		}
-		fmt.Println(string(wat))
+
+		if c.Bool("hello") {
+			wat, err := api.BuildFile(
+				config.DefaultConfig(),
+				"hello.wa", "func main() { println(123) }",
+			)
+			if err != nil {
+				if len(wat) != 0 {
+					fmt.Println(string(wat))
+				}
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println(string(wat))
+			os.Exit(0)
+		}
+
+		fmt.Println("...dev...")
 		return nil
 	},
+}
+
+func RunCountCodeLines() {
+	var dir = "."
+	if len(os.Args) > 1 {
+		dir = os.Args[1]
+	}
+
+	total := 0
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal("filepath.Walk: ", err)
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			log.Fatal("ioutil.ReadFile: ", err)
+		}
+		if needSkip(path, data) {
+			return nil
+		}
+
+		n := countLine(data)
+		fmt.Println(path, n)
+
+		total += n
+		return nil
+	})
+	fmt.Printf("total %d\n", total)
+}
+
+func needSkip(path string, data []byte) bool {
+	if !hasExt(path, ".wa", ".go", ".ws") {
+		return true
+	}
+	if strings.Contains(path, "3rdparty") {
+		return true
+	}
+	if bytes.Contains(data, []byte("The Go Authors")) {
+		return true
+	}
+
+	return false
+}
+
+func countLine(data []byte) int {
+	return bytes.Count(data, []byte("\n"))
+}
+
+func hasExt(name string, extensions ...string) bool {
+	for _, ext := range extensions {
+		if strings.HasSuffix(name, ext) {
+			return true
+		}
+	}
+	return false
 }
