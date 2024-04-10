14b3727c71977da015d3026eb2d2e10cdf1a971e
3dgen <476582@qq.com>
2024-04-10 16:37:53 +0800

    增加 net 包 Fetch 函数

---
+++ waroot/src/net/net.import.js
@@ -0,0 +1,25 @@
+net: new function() {
+  this.fetch_blob = (tid, res_b, res_d, res_l) => {
+    let resource = app._mem_util.get_string(res_d, res_l)
+    let status = 0
+    fetch(resource)
+      .then((response) => {
+        status = response.status;
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.arrayBuffer();
+      })
+      .then((data) => {
+        let u8a = new Uint8Array(data)
+        let slice = app._mem_util.set_bytes(u8a);
+        app._wasm_inst.exports["net.onFetchBlobDone"](tid, 1, status, ...slice);        
+        app._mem_util.block_release(slice[0]);
+      })
+      .catch((err) => {
+        app._wasm_inst.exports["net.onFetchBlobDone"](tid, 0, status, 0, 0, 0, 0);
+        console.log(err)
+      })
+  }
+
+},
