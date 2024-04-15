29591d061cc046a191ef76a72ed0f115c34527d5
3dgen <476582@qq.com>
2024-04-15 17:05:37 +0800

    增加 net/FetchImage

--- waroot/src/net/net.import.js
+++ waroot/src/net/net.import.js
@@ -11,7 +11,7 @@ net: new function() {
         return response.arrayBuffer();
       })
       .then((data) => {
-        let u8a = new Uint8Array(data)
+        let u8a = new Uint8Array(data);
         let slice = app._mem_util.set_bytes(u8a);
         app._wasm_inst.exports["net.onFetchBlobDone"](tid, 1, status, ...slice);        
         app._mem_util.block_release(slice[0]);
@@ -22,4 +22,27 @@ net: new function() {
       })
   }
 
+  this.fetch_image = (tid, res_b, res_d, res_l) => {
+    let resource = app._mem_util.get_string(res_d, res_l)
+    let status = 0
+    fetch(resource)
+      .then((response) => {
+        status = response.status;
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        return createImageBitmap(blob);
+      })
+      .then((img) => {
+        let imgid = app._extobj.insert_obj(img);
+        app._wasm_inst.exports["net.onFetchImageDone"](tid, 1, status, imgid);
+      })
+      .catch((err) => {
+        app._wasm_inst.exports["net.onFetchImageDone"](tid, 0, status, 0);
+        console.log(err)
+      })
+  }
 },
