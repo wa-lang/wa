009fa3fc898e9a7ffb9d381f78f74397553f0557
chai2010 <chaishushan@gmail.com>
2024-04-30 21:39:31 +0800

    lsp hover 初步可工作

--- internal/lsp/server_hover.go
+++ internal/lsp/server_hover.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"wa-lang.org/wa/internal/ast"
 	"wa-lang.org/wa/internal/lsp/protocol"
 	"wa-lang.org/wa/internal/token"
 )
@@ -44,37 +45,53 @@ func (p *LSPServer) Hover(ctx context.Context, params *protocol.HoverParams) (*p
 	}
 
 	mapper := protocol.NewMapper(params.TextDocument.URI, content)
+	var base int
 
 	var pos = token.NoPos
+	var file *ast.File
 	for _, f := range mainPkg.Files {
 		// todo(chai): fix f.Pos() 位置
 		p.logger.Println("file:", module.program.Fset.Position(f.Pos()), f.Name)
-		if module.program.Fset.File(f.Pos()).Name() == baseName {
+		tokFile := module.program.Fset.File(f.Pos())
+		if tokFile.Name() == baseName {
 			off, err := mapper.PositionOffset(params.Position)
 			if err != nil {
 				return nil, err
 			}
-			pos = f.Pos() + token.Pos(off)
+			base = tokFile.Base()
+			pos = token.Pos(tokFile.Base() + off)
+			file = f
 			break
 		}
 	}
-
 	if pos == token.NoPos {
 		return nil, fmt.Errorf("no pos")
 	}
 
-	scope := mainPkg.Pkg.Scope().Innermost(pos)
-	for i, name := range scope.Names() {
-		obj := scope.Lookup(name)
-		p.logger.Println(i, name, obj.Pos())
+	var objIdent *ast.Ident
+	ast.Inspect(file, func(n ast.Node) bool {
+		if n == nil {
+			return false
+		}
+		if pos < n.Pos() || pos > n.End() {
+			return false
+		}
+		if x, ok := n.(*ast.Ident); ok {
+			objIdent = x
+			return false
+		}
+		return true
+	})
+	if objIdent == nil {
+		return nil, nil
 	}
 
-	obj := scope.LookupByPos(pos)
+	obj := mainPkg.Info.ObjectOf(objIdent)
 	if obj == nil {
 		return nil, nil
 	}
 
-	rng, err := mapper.OffsetRange(int(obj.Pos()), int(obj.Node().End()))
+	rng, err := mapper.OffsetRange(int(objIdent.Pos())-base, int(objIdent.End())-base)
 	if err != nil {
 		return nil, err
 	}
@@ -82,7 +99,7 @@ func (p *LSPServer) Hover(ctx context.Context, params *protocol.HoverParams) (*p
 	reply := &protocol.Hover{
 		Contents: protocol.MarkupContent{
 			Kind:  "wa",
-			Value: obj.String(),
+			Value: obj.Type().String() + " @ " + module.program.Fset.Position(obj.Pos()).String(),
 		},
 		Range: rng,
 	}
