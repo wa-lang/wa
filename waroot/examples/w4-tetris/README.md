dcd08d3096c9d5f44e98b3e55890f58d2f5ba049
chai2010 <chaishushan@gmail.com>
2024-09-02 22:54:25 +0800

    准备移植俄罗斯方块游戏

---
+++ waroot/examples/w4-tetris/README.md
@@ -0,0 +1,6 @@
+# WASM4 例子
+
+https://wasm4.org/docs/
+
+Based on this version in C: https://github.com/aduros/wasm4/tree/main/examples/watris
+
---
+++ waroot/examples/w4-tetris/src/utils.h
@@ -0,0 +1,8 @@
+#pragma once
+
+char* itoa (char* dest, int i);
+
+int rand ();
+
+int min (int a, int b) { return (a < b) ? a : b; }
+int max (int a, int b) { return (a > b) ? a : b; }
