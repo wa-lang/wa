ee4b3644321a82a5732333a0076bcae4b6214f2c
chai2010 <chaishushan@gmail.com>
2025-02-19 15:31:15 +0800

    3rdparty/purego:   补充例子

---
+++ internal/3rdparty/purego/examples/libc/main.go
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2022 The Ebitengine Authors
+
+//go:build darwin || freebsd || linux || windows
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"wa-lang.org/wa/internal/3rdparty/purego"
+)
+
+func getSystemLibrary() string {
+	switch runtime.GOOS {
+	case "darwin":
+		return "/usr/lib/libSystem.B.dylib"
+	case "linux":
+		return "libc.so.6"
+	case "freebsd":
+		return "libc.so.7"
+	case "windows":
+		return "ucrtbase.dll"
+	default:
+		panic(fmt.Errorf("GOOS=%s is not supported", runtime.GOOS))
+	}
+}
+
+func main() {
+	libc, err := openLibrary(getSystemLibrary())
+	if err != nil {
+		panic(err)
+	}
+	var puts func(string)
+	purego.RegisterLibFunc(&puts, libc, "puts")
+	puts("Calling C from Go without Cgo!")
+}
---
+++ internal/3rdparty/purego/examples/objc/main_darwin.go
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2022 The Ebitengine Authors
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+
+	"wa-lang.org/wa/internal/3rdparty/purego/objc"
+)
+
+var (
+	sel_new    = objc.RegisterName("new")
+	sel_init   = objc.RegisterName("init")
+	sel_setBar = objc.RegisterName("setBar:")
+	sel_bar    = objc.RegisterName("bar")
+)
+
+func BarInit(id objc.ID, cmd objc.SEL) objc.ID {
+	return id.SendSuper(cmd)
+}
+
+func main() {
+	// This struct is equivalent to the following Objective-C definition.
+	//
+	// @interface BarObject : NSObject <NSDelegateWindow>
+	// @property (readwrite) bar int
+	// @end
+	class, err := objc.RegisterClass(
+		"BarObject",
+		objc.GetClass("NSObject"),
+		[]*objc.Protocol{
+			objc.GetProtocol("NSDelegateWindow"),
+		},
+		[]objc.FieldDef{
+			{
+				Name:      "bar",
+				Type:      reflect.TypeOf(int(0)),
+				Attribute: objc.ReadWrite,
+			},
+		},
+		[]objc.MethodDef{
+			{
+				Cmd: sel_init,
+				Fn:  BarInit,
+			},
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	object := objc.ID(class).Send(sel_new)
+	object.Send(sel_setBar, 123)
+	bar := int(object.Send(sel_bar))
+	fmt.Println(bar)
+	// Output: 123
+}
