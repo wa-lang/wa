f1e64e52519f177124318fbc3c40485c442397ae
chai2010 <chaishushan@gmail.com>
2025-03-03 10:55:22 +0800

    wasm/js 目标的凹语言编译器增加目录系统支持

--- main_js.go
+++ main_js.go
@@ -7,8 +7,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"syscall/js"
+	"testing/fstest"
+	"time"
 
 	"wa-lang.org/wa/api"
 	"wa-lang.org/wa/internal/backends/compiler_wat"
@@ -32,12 +35,27 @@ func runMain() {
 	// __WA_CODE__ 代码内容
 	waName := getJsValue(window, "__WA_FILE_NAME__", "hello.wa")
 	waCode := getJsValue(window, "__WA_CODE__", "// no code")
+	waVfsJson := getJsValue(window, "__WA_VFS_JSON__", "")
 
 	waClearError()
 
-	outWat := waGenerateWat(waName, waCode)
-	outFmt := waFormatCode(waName, waCode)
-	outWasm := waGenerateWasm(waName, outWat)
+	var (
+		outWat  string
+		outFmt  string
+		outWasm []byte
+	)
+
+	if waVfsJson == "" {
+		outWat = waGenerateWat(waName, waCode)
+		outFmt = waFormatCode(waName, waCode)
+		outWasm = waGenerateWasm(waName, outWat)
+	} else {
+		vfs := waLoadVFSFromJson(waVfsJson, waName)
+
+		outWat = waGenerateWatVFS(vfs, waName)
+		outFmt = waFormatCode(waName, waCode)
+		outWasm = waGenerateWasm(waName, outWat)
+	}
 
 	if !window.IsNull() && !window.IsUndefined() {
 		window.Set("__WA_WAT__", outWat)
@@ -51,6 +69,37 @@ func runMain() {
 		window.Set("__WA_ERROR__", waGetErrorText())
 	} else {
 		fmt.Println(outWat)
+	}
+}
+
+// 从 vfs json 字符串构建 vfs
+func waLoadVFSFromJson(waVfsJson, filename string) *config.PkgVFS {
+	if waGetError() != nil {
+		return nil
+	}
+
+	// json解析为扁平的map
+	var m map[string]string
+	if err := json.Unmarshal([]byte(waVfsJson), &m); err != nil {
+		waSetError(err)
+	}
+
+	// 构建 map fs
+	mapfs := make(fstest.MapFS)
+	now := time.Now()
+
+	// 遍历 JSON 文件路径
+	for filePath, content := range m {
+		mapfs[filePath] = &fstest.MapFile{
+			Data:    []byte(content),
+			Mode:    0644,
+			ModTime: now,
+		}
+	}
+
+	// 设置 app 文件系统
+	return &config.PkgVFS{
+		App: mapfs,
 	}
 }
 
@@ -82,6 +131,20 @@ func waGenerateWat(filename, code string) string {
 	return string(wat)
 }
 
+func waGenerateWatVFS(vfs *config.PkgVFS, filename string) string {
+	cfg := api.DefaultConfig()
+	cfg.Target = api.WaOS_js
+
+	wat, err := waBuildVFS(vfs, cfg, filename)
+	if err != nil {
+		if waGetError() == nil {
+			waSetError(err)
+		}
+		return ""
+	}
+	return string(wat)
+}
+
 func waFormatCode(filename, code string) string {
 	newCode, err := api.FormatCode(filename, code)
 	if err != nil {
@@ -93,11 +156,23 @@ func waFormatCode(filename, code string) string {
 	return newCode
 }
 
-// 构建 wat 目标
+// 从文件构建 wat 目标
 func waBuildFile(cfg *config.Config, filename string, src interface{}) (wat []byte, err error) {
 	prog, err := loader.LoadProgramFile(cfg, filename, src)
 	if err != nil || prog == nil {
-		logger.Tracef(&config.EnableTrace_api, "LoadProgramVFS failed, err = %v", err)
+		logger.Tracef(&config.EnableTrace_api, "loader.LoadProgramFile failed, err = %v", err)
+		return nil, err
+	}
+
+	watOut, err := compiler_wat.New().Compile(prog)
+	return []byte(watOut), err
+}
+
+// 从文件系统构建 wat 目标
+func waBuildVFS(vfs *config.PkgVFS, cfg *config.Config, pkgpath string) (wat []byte, err error) {
+	prog, err := loader.LoadProgramVFS(vfs, cfg, pkgpath)
+	if err != nil || prog == nil {
+		logger.Tracef(&config.EnableTrace_api, "loader.LoadProgramVFS failed, err = %v", err)
 		return nil, err
 	}
 
