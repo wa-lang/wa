b0b4eefdb9959994126c2a7d3b1c4391b092925d
chai2010 <chaishushan@gmail.com>
2025-12-16 20:06:37 +0800

    完善龙芯原始汇编示例

--- internal/native/examples/hello-loong64-linux-v0/hello_la64.s
+++ internal/native/examples/hello-loong64-linux-v0/hello_la64.s
@@ -1,24 +1,24 @@
 # Copyright (C) 2025 武汉凹语言科技有限公司
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-.text
-.align 2
-.globl _start
-
-.equ SYS_write, 64
-.equ SYS_exit, 93
-.equ STDOUT, 1
-
 .data
 msg:
     .asciz "Hello, LoongArch 64!\n"
 len = . - msg
 
+.equ STDOUT, 1
+.equ SYS_write, 64
+.equ SYS_exit, 93
+
+.text
+.align 2
+.globl _start
+
 _start:
     # SYS_write(STDOUT, msg, len)
     addi.d    $a0, $zero, STDOUT
-    pcaddu12i $a1, %pcrel_hi(msg)
-    addi.d    $a1, $a1, %pcrel_lo(msg)
+    pcalau12i $a1, %pc_hi20(msg)
+    addi.d    $a1, $a1, %pc_lo12(msg)
     addi.d    $a2, $zero, len
     addi.d    $a7, $zero, SYS_write
     syscall   0
