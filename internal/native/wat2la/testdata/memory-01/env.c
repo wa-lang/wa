d430aa604fcd5ccdec4c77943e7c0d9b34798bf7
chai2010 <chaishushan@gmail.com>
2026-01-17 06:39:26 +0800

    wat2la: 补充龙芯的测试代码

---
+++ internal/native/wat2la/testdata/memory-01/env.c
@@ -0,0 +1,13 @@
+// Copyright (C) 2026 武汉凹语言科技有限公司
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
+extern int64_t wat2x64_Memory_addr __asm__(".Memory.addr");
+
+int64_t wat2x64_syscall_write(int64_t fd, int64_t ptr, int64_t size) {
+    printf("loong64: wat2x64_syscall_write: %ld, %ld, %ld\n", fd, ptr, size);
+    return write(fd, (void *)(wat2x64_Memory_addr+ptr), size);
+}
