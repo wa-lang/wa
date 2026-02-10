64aba4d91f80c5555b8042c88a73a809c52636b1
chai2010 <chaishushan@gmail.com>
2026-02-10 17:39:45 +0800

    wat2riscv: 中文版宿主汇编改成中文指令

--- internal/native/wat2rv/assets/native-env-linux-rv64.wz.s
+++ internal/native/wat2rv/assets/native-env-linux-rv64.wz.s
@@ -1,198 +1,196 @@
 # Copyright (C) 2026 武汉凹语言科技有限公司
 # SPDX-License-Identifier: AGPL-3.0-或-later
 
-# TODO: 改为RISCV中文汇编
-
 声明 .Wa.Memory.addr
 
 # void _Wa_Import_syscall_write (int fd, uint32_t ptr, int32_t len)
 函数 .Wa.Import.syscall.write:
-    auipc   t0, %相对高位(.Wa.Memory.addr)
-    ld      t0, %相对低位(.Wa.Import.syscall.write)(t0)
+    计加常 暂甲格, %相对高位(.Wa.Memory.addr)
+    读长整 暂甲格, %相对低位(.Wa.Import.syscall.write)(暂甲格)
 
-    # a0 = fd
-    add a1, a1, t0 # a1 = base + ptr
-    # a2 = len
-    addi a7, zero, 64 # sys_write
-    ecall
+    # 参甲格 = fd
+    某加某 参乙格, 参乙格, 暂甲格 # 参乙格 = base + ptr
+    # 参丙格 = len
+    某加常 参辛格, 零格, 64 # sys_write
+    调用某
 
-    jalr zero, 0(ra)
+    转某址 零格, 0(回格)
 完毕
 
 
 # int _Wa_Runtime_write(int fd, void *buf, int count)
 函数 .Wa.Runtime.write:
-    addi a7, zero, 64 # sys_write
-    ecall
-    jalr zero, 0(ra)
+    某加常 参辛格, 零格, 64 # sys_write
+    调用某
+    转某址 零格, 0(回格)
 完毕
 
 
 # void _Wa_Runtime_exit(int status)
 函数 .Wa.Runtime.exit:
-    addi a7, zero, 93 # sys_exit
-    ecall
+    某加常 参辛格, 零格, 93 # sys_exit
+    调用某
 完毕
 
 
 # void* _Wa_Runtime_malloc(int size)
 函数 .Wa.Runtime.malloc:
-    addi  a1, a0, 0     # length = size
-    addi  a0, zero, 0   # addr = NULL
-    addi  a2, zero, 3   # prot = PROT_READ | PROT_WRITE
-    addi  a3, zero, 34  # flags = MAP_PRIVATE | MAP_ANONYMOUS
-    addi  a4, zero, -1  # fd = -1
-    addi  a5, zero, 0   # offset = 0
-    addi  a7, zero, 222 # sys_mmap (222)
-    ecall
-    jalr  zero, 0(ra)
+    某加常 参乙格, 参甲格, 0     # length = size
+    某加常 参甲格, 零格, 0   # addr = NULL
+    某加常 参丙格, 零格, 3   # prot = PROT_READ | PROT_WRITE
+    某加常 参丁格, 零格, 34  # flags = MAP_PRIVATE | MAP_ANONYMOUS
+    某加常 参戊格, 零格, -1  # fd = -1
+    某加常 参己格, 零格, 0   # offset = 0
+    某加常 参辛格, 零格, 222 # sys_mmap (222)
+    调用某
+    转某址 零格, 0(回格)
 完毕
 
 
 # void* _Wa_Runtime_memcpy(void* dst, const void* src, int n)
 函数 .Wa.Runtime.memcpy:
-    addi t0, a0, 0 # 备份 dst
-    beq  a2, zero, .Wa.L.memcpy.done
+    某加常 暂甲格, 参甲格, 0 # 备份 dst
+    若等转 参丙格, 零格, .Wa.L.memcpy.done
 .Wa.L.memcpy.loop:
-    lb   t1, 0(a1) # 字节读取
-    sb   t1, 0(a0) # 字节写入
-    addi a0, a0, 1
-    addi a1, a1, 1
-    addi a2, a2, -1
-    bne  a2, zero, .Wa.L.memcpy.loop
+    读微整 暂乙格, 0(参乙格) # 字节读取
+    写微整 暂乙格, 0(参甲格) # 字节写入
+    某加常 参甲格, 参甲格, 1
+    某加常 参乙格, 参乙格, 1
+    某加常 参丙格, 参丙格, -1
+    若异转 参丙格, 零格, .Wa.L.memcpy.loop
 .Wa.L.memcpy.done:
-    addi a0, t0, 0 # 返回 dst
-    jalr zero, 0(ra)
+    某加常 参甲格, 暂甲格, 0 # 返回 dst
+    转某址 零格, 0(回格)
 完毕
 
 
 # void* _Wa_Runtime_memmove(void* dst, const void* src, int n)
 函数 .Wa.Runtime.memmove:
-    beq a0, a1, .Wa.L.memmove.done
-    # 如果 a0 < a1 (无符号比较), 跳转到向前拷贝
-    bltu a0, a1, .Wa.Runtime.memcpy
+    若等转 参甲格, 参乙格, .Wa.L.memmove.done
+    # 如果 参甲格 < 参乙格 (无符号比较), 跳转到向前拷贝
+    若低转 参甲格, 参乙格, .Wa.Runtime.memcpy
 
     # 后向拷贝 (dst > src)
-    addi t0, a0, 0
-    add  a0, a0, a2
-    add  a1, a1, a2
+    某加常 暂甲格, 参甲格, 0
+    某加某 参甲格, 参甲格, 参丙格
+    某加某 参乙格, 参乙格, 参丙格
 .Wa.L.memmove.back_loop:
-    beq  a2, zero, .Wa.L.memmove.ret
-    addi a0, a0, -1
-    addi a1, a1, -1
-    lb   t1, 0(a1)
-    sb   t1, 0(a0)
-    addi a2, a2, -1
-    jal  zero, .Wa.L.memmove.back_loop
+    若等转 参丙格, 零格, .Wa.L.memmove.ret
+    某加常 参甲格, 参甲格, -1
+    某加常 参乙格, 参乙格, -1
+    读微整 暂乙格, 0(参乙格)
+    写微整 暂乙格, 0(参甲格)
+    某加常 参丙格, 参丙格, -1
+    转常址 零格, .Wa.L.memmove.back_loop
 .Wa.L.memmove.ret:
-    addi a0, t0, 0
+    某加常 参甲格, 暂甲格, 0
 .Wa.L.memmove.done:
-    jalr zero, 0(ra)
+    转某址 零格, 0(回格)
 完毕
 
 
 # void* _Wa_Runtime_memset(void* s, int c, int n)
 函数 .Wa.Runtime.memset:
-    addi t0, a0, 0
-    beq  a2, zero, .Wa.L.memset.done
+    某加常 暂甲格, 参甲格, 0
+    若等转 参丙格, 零格, .Wa.L.memset.done
 .Wa.L.memset.loop:
-    sb   a1, 0(a0)
-    addi a0, a0, 1
-    addi a2, a2, -1
-    bne  a2, zero, .Wa.L.memset.loop
+    写微整 参乙格, 0(参甲格)
+    某加常 参甲格, 参甲格, 1
+    某加常 参丙格, 参丙格, -1
+    若异转 参丙格, 零格, .Wa.L.memset.loop
 .Wa.L.memset.done:
-    addi a0, t0, 0
-    jalr zero, 0(ra)
+    某加常 参甲格, 暂甲格, 0
+    转某址 零格, 0(回格)
 完毕
 
 
 # void _Wa_Import_syscall_linux_print_str (uint32_t ptr, int32_t len)
 函数 .Wa.Import.syscall_linux.print_str:
-    auipc t0, %相对高位(.Wa.Memory.addr)
-    ld    t0, %相对低位(.Wa.Import.syscall_linux.print_str)(t0)
+    计加常 暂甲格, %相对高位(.Wa.Memory.addr)
+    读长整    暂甲格, %相对低位(.Wa.Import.syscall_linux.print_str)(暂甲格)
 
-    # a0 = ptr, a1 = len
-    addi a2, a1, 0    # count = len
-    add  a1, t0, a0   # buf = base + ptr
-    addi a0, zero, 1  # fd = stdout (1)
-    addi a7, zero, 64 # sys_write
-    ecall
-    jalr zero, 0(ra)
+    # 参甲格 = ptr, 参乙格 = len
+    某加常 参丙格, 参乙格, 0    # count = len
+    某加某 参乙格, 暂甲格, 参甲格   # buf = base + ptr
+    某加常 参甲格, 零格, 1  # fd = stdout (1)
+    某加常 参辛格, 零格, 64 # sys_write
+    调用某
+    转某址 零格, 0(回格)
 完毕
 
 
 # void _Wa_Import_syscall_linux_proc_exit(int32_t code)
 函数 .Wa.Import.syscall_linux.proc_exit:
-    jal zero, .Wa.Runtime.exit
+    转常址 零格, .Wa.Runtime.exit
 完毕
 
 
 # void _Wa_Import_syscall_linux_print_rune(int32_t c)
 函数 .Wa.Import.syscall_linux.print_rune:
-    addi sp, sp, -16
-    sd   ra, 8(sp)
-    sd   s0, 0(sp)
-    addi s0, sp, 0
-    addi sp, sp, -16
+    某加常 栈格, 栈格, -16
+    写长整 回格, 8(栈格)
+    写长整 守甲格, 0(栈格)
+    某加常 守甲格, 栈格, 0
+    某加常 栈格, 栈格, -16
 
-    sb a0, 0(sp)
+    写微整 参甲格, 0(栈格)
 
-    addi a0, zero, 1  # arg.0: stdout
-    addi a1, sp, 0    # arg.1: buffer
-    addi a2, zero, 1  # arg.2: count
-    addi a7, zero, 64 # sys_write
-    ecall
+    某加常 参甲格, 零格, 1  # arg.0: stdout
+    某加常 参乙格, 栈格, 0    # arg.1: buffer
+    某加常 参丙格, 零格, 1  # arg.2: count
+    某加常 参辛格, 零格, 64 # sys_write
+    调用某
 
-    addi sp, s0, 0
-    ld   s0, 0(sp)
-    ld   ra, 8(sp)
-    ld   ra, 8(sp)
-    addi sp, sp, 16
+    某加常 栈格, 守甲格, 0
+    读长整 守甲格, 0(栈格)
+    读长整 回格, 8(栈格)
+    读长整 回格, 8(栈格)
+    某加常 栈格, 栈格, 16
 完毕
 
 
 # void _Wa_Import_syscall_linux_print_i64(int64_t val)
 函数 .Wa.Import.syscall_linux.print_i64:
-    addi sp, sp, -16
-    sd   ra, 8(sp)
-    sd   s0, 0(sp)
-    addi s0, sp, 0
-    addi sp, sp, -32
+    某加常 栈格, 栈格, -16
+    写长整 回格, 8(栈格)
+    写长整 守甲格, 0(栈格)
+    某加常 守甲格, 栈格, 0
+    某加常 栈格, 栈格, -32
 
-    addi t0, a0, 0    # t0 = 工作变量 (val)
-    addi t1, s0, -1   # t1 为缓冲区指针 (从后往前填)
-    addi t2, zero, 10 # 除数
+    某加常 暂甲格, 参甲格, 0    # 暂甲格 = 工作变量 (val)
+    某加常 暂乙格, 守甲格, -1   # 暂乙格 为缓冲区指针 (从后往前填)
+    某加常 暂丙格, 零格, 10 # 除数
 
     # 1. 处理负数
-    bge t0, zero, .Wa.L.syscall_linux.print_i64.convert
-    sub t0, zero, t0 # t0 = abs(t0)
+    若大转 暂甲格, 零格, .Wa.L.syscall_linux.print_i64.convert
+    某减某 暂甲格, 零格, 暂甲格 # 暂甲格 = abs(暂甲格)
 
 .Wa.L.syscall_linux.print_i64.convert:
-    div  t3, t0, t2 # t3 = 商
-    rem  t4, t0, t2 # t4 = 余数
-    addi t4, t4, 48 # 加上 '0' 的 ASCII 码
-    sb   t4, 0(t1)  # 存入缓冲区
-    addi t1, t1, -1 # 指针前移
-    addi t0, t3, 0  # 更新待处理的数字
-    bne  t0, zero, .Wa.L.syscall_linux.print_i64.convert
+    某除某 暂丁格, 暂甲格, 暂丙格 # 暂丁格 = 商
+    某模某 暂戊格, 暂甲格, 暂丙格 # 暂戊格 = 余数
+    某加常 暂戊格, 暂戊格, 48 # 加上 '0' 的 ASCII 码
+    写微整 暂戊格, 0(暂乙格)  # 存入缓冲区
+    某加常 暂乙格, 暂乙格, -1 # 指针前移
+    某加常 暂甲格, 暂丁格, 0  # 更新待处理的数字
+    若异转 暂甲格, 零格, .Wa.L.syscall_linux.print_i64.convert
 
     # 2. 补负号
-    bge  a0, zero, .Wa.L.syscall_linux.print_i64.print
-    addi t4, zero, 45 # '-'
-    sb   t4, 0(t1)
-    addi t1, t1, -1
+    若大转 参甲格, 零格, .Wa.L.syscall_linux.print_i64.print
+    某加常 暂戊格, 零格, 45 # '-'
+    写微整 暂戊格, 0(暂乙格)
+    某加常 暂乙格, 暂乙格, -1
 
 .Wa.L.syscall_linux.print_i64.print:
-    addi a0, zero, 1  # arg.0: stdout
-    addi a1, t1, 1    # arg.1: buffer
-    sub  a2, s0, a1   # arg.2: count
-    addi a7, zero, 64 # sys_write
-    ecall
+    某加常 参甲格, 零格, 1  # arg.0: stdout
+    某加常 参乙格, 暂乙格, 1    # arg.1: buffer
+    某减某 参丙格, 守甲格, 参乙格   # arg.2: count
+    某加常 参辛格, 零格, 64 # sys_write
+    调用某
 
-    addi sp, s0, 0
-    ld   s0, 0(sp)
-    ld   ra, 8(sp)
-    addi sp, sp, 16
-    jalr zero, 0(ra)
+    某加常 栈格, 守甲格, 0
+    读长整 守甲格, 0(栈格)
+    读长整 回格, 8(栈格)
+    某加常 栈格, 栈格, 16
+    转某址 零格, 0(回格)
 完毕
 
