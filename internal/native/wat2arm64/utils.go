82e834e934b8faed0e33e284877348f3d2d64c88
chai2010 <chaishushan@gmail.com>
2026-02-10 09:20:24 +0800

    native/wat2arm64: 添加ARM64版本的骨架代码

---
+++ internal/native/wat2arm64/utils.go
@@ -0,0 +1,120 @@
+// Copyright (C) 2025 武汉凹语言科技有限公司
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package wat2arm64
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"unsafe"
+
+	nativeast "wa-lang.org/wa/internal/native/ast"
+	nativetok "wa-lang.org/wa/internal/native/token"
+	"wa-lang.org/wa/internal/wat/ast"
+	"wa-lang.org/wa/internal/wat/token"
+)
+
+func assert(condition bool, args ...interface{}) {
+	if !condition {
+		if msg := fmt.Sprint(args...); msg != "" {
+			panic(fmt.Sprintf("assert failed, %s", msg))
+		} else {
+			panic("assert failed")
+		}
+	}
+}
+
+func unreachable() {
+	panic("unreachable")
+}
+
+func wat2nativeType(typ token.Token) nativetok.Type {
+	switch typ {
+	case token.I32:
+		return nativetok.I32
+	case token.I64:
+		return nativetok.I64
+	case token.F32:
+		return nativetok.F32
+	case token.F64:
+		return nativetok.F64
+	default:
+		panic("unreachable")
+	}
+}
+
+// 转化为本地函数结构(不含指令)
+func wat2nativeFunc(fnName string, fnType *ast.FuncType, fnLocals []ast.Field) *nativeast.Func {
+	fnNative := &nativeast.Func{
+		Name: fnName,
+		Type: &nativeast.FuncType{
+			Args:   make([]*nativeast.Local, len(fnType.Params)),
+			Return: make([]*nativeast.Local, len(fnType.Results)),
+		},
+		Body: &nativeast.FuncBody{
+			Locals: make([]*nativeast.Local, len(fnLocals)),
+		},
+	}
+	for i, arg := range fnType.Params {
+		fnNative.Type.Args[i] = &nativeast.Local{
+			Name: arg.Name,
+			Type: wat2nativeType(arg.Type),
+			Cap:  1,
+		}
+	}
+	for i, typ := range fnType.Results {
+		fnNative.Type.Return[i] = &nativeast.Local{
+			Name: fmt.Sprintf("%s%d", kFuncRetNamePrefix, i),
+			Type: wat2nativeType(typ),
+			Cap:  1,
+		}
+	}
+	for i, local := range fnLocals {
+		fnNative.Body.Locals[i] = &nativeast.Local{
+			Name: local.Name,
+			Type: wat2nativeType(local.Type),
+			Cap:  1,
+		}
+	}
+	return fnNative
+}
+
+func isSameInstList(s1, s2 []ast.Instruction) bool {
+	h1 := (*reflect.SliceHeader)(unsafe.Pointer(&s1))
+	h2 := (*reflect.SliceHeader)(unsafe.Pointer(&s2))
+	return h1.Data == h2.Data
+}
+
+func fixName(s string) string {
+	if strings.ContainsAny(s, "/`") {
+		s = strings.ReplaceAll(s, "/", ".")
+		s = strings.ReplaceAll(s, "`", ".")
+	}
+	return s
+}
+
+// C 语言中 \x 转义序列是“贪婪”的, 会一直读取尽可能多的十六进制字符作为转义值的一部分，不会自动终止。
+// 因此 \x000a 会被解析为 \x0a, 而不是 \x00 和 "0a" 字符串
+// 解决的办法是通过字符串强制切割: \x00""0a
+
+// 对于 gas 汇编中, 采用3个数字的八进制转义
+func toGasString(dataValue []byte) string {
+	var sb strings.Builder
+	for _, b := range dataValue {
+		// 仅保留安全的 ASCII 可打印字符, 避开双引号 " 和反斜杠 \
+		if b >= 32 && b <= 126 && b != '"' && b != '\\' {
+			sb.WriteByte(b)
+		} else {
+			sb.WriteString(fmt.Sprintf("\\%03o", b))
+		}
+	}
+	return sb.String()
+}
+
+// 把 val 的低 bit 位当作一个有符号数扩展成 int32
+func i32SignExtend(val uint32, bit uint) int32 {
+	// 1. 先左移, 把符号位移到最高位
+	// 2. 再算术右移(保持符号), 补全剩余的高位
+	return int32(val) << (32 - bit) >> (32 - bit)
+}
---
+++ internal/native/wat2arm64/wat2arm64.go
@@ -0,0 +1,131 @@
+// Copyright (C) 2025 武汉凹语言科技有限公司
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package wat2arm64
+
+import (
+	"bytes"
+	"fmt"
+
+	"wa-lang.org/wa/internal/config"
+	"wa-lang.org/wa/internal/native/abi"
+	"wa-lang.org/wa/internal/wasm"
+	watast "wa-lang.org/wa/internal/wat/ast"
+	watparser "wa-lang.org/wa/internal/wat/parser"
+)
+
+const DebugMode = false
+
+// Wat程序转译到 龙芯汇编
+func Wat2LA64(filename string, source []byte, isZhLang bool) (m *watast.Module, code []byte, err error) {
+	m, err = watparser.ParseModule(filename, source)
+	if err != nil {
+		return m, nil, err
+	}
+
+	worker := newWat2LAWorker(filename, m, isZhLang)
+	code, err = worker.BuildProgram()
+	return
+}
+
+type wat2arm64Worker struct {
+	m *watast.Module
+
+	filename string
+	cpuType  abi.CPUType
+	isZhLang bool
+
+	inlinedTypeIndices []*inlinedTypeIndex
+	inlinedTypes       []*wasm.FunctionType
+
+	fnWasmR0Base      int // 当前函数的WASM栈R0位置
+	fnMaxCallArgsSize int // 调用子函数需要的最大空间
+
+	nextId int64 // 用于生成唯一ID
+
+	trace bool // 调试开关
+}
+
+type inlinedTypeIndex struct {
+	section    wasm.SectionID
+	idx        wasm.Index
+	inlinedIdx wasm.Index
+}
+
+func newWat2LAWorker(filename string, mWat *watast.Module, isZhLang bool) *wat2arm64Worker {
+	p := &wat2arm64Worker{
+		m:        mWat,
+		cpuType:  abi.RISCV64, // 暂时面向linux环境
+		isZhLang: isZhLang,
+		filename: filename,
+		trace:    DebugMode,
+	}
+
+	if config.EnableTrace_wat2xx {
+		p.trace = true
+	}
+
+	// 如果 start 字段为空, 则尝试用 _start 导出函数替代
+	if p.m.Start == "" {
+		for _, fn := range p.m.Funcs {
+			if fn.ExportName == "_start" {
+				p.m.Start = fn.Name
+			}
+		}
+	}
+
+	return p
+}
+
+func (p *wat2arm64Worker) BuildProgram() (code []byte, err error) {
+	var out bytes.Buffer
+
+	fmt.Fprintf(&out, "# 源文件: %s, ABI: %s\n", p.filename, p.cpuType)
+	fmt.Fprintf(&out, "# 自动生成的代码, 不要手动修改!!!\n\n")
+
+	// 生成运行时函数
+	p.buildRuntimeHead(&out)
+
+	// 导入函数
+	if err := p.buildImport(&out); err != nil {
+		return nil, err
+	}
+
+	// 构建内存
+	if err := p.buildMemory(&out); err != nil {
+		return nil, err
+	}
+
+	// 构建表格
+	if err := p.buildTable(&out); err != nil {
+		return nil, err
+	}
+
+	// 构建全局变量
+	if err := p.buildGlobal(&out); err != nil {
+		return nil, err
+	}
+
+	// 启动函数
+	if err := p.buildStart(&out); err != nil {
+		return nil, err
+	}
+
+	// 生成运行时函数
+	if err := p.buildRuntimeImpl(&out); err != nil {
+		return nil, err
+	}
+
+	// 构建函数
+	if err := p.buildFuncs(&out); err != nil {
+		return nil, err
+	}
+
+	return out.Bytes(), nil
+}
+
+func (p *wat2arm64Worker) genNextId() string {
+	nextId := p.nextId
+	p.nextId++
+	return fmt.Sprintf("%08X", nextId)
+}
