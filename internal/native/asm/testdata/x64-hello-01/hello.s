ea29e2a880d9a64e3557ee0651e92a24e939deea
chai2010 <chaishushan@gmail.com>
2026-02-12 21:25:57 +0800

    native/asm: 添加一个x64最简测试

---
+++ internal/native/asm/testdata/x64-hello-01/hello.s
@@ -0,0 +1,17 @@
+# Copyright (C) 2026 武汉凹语言科技有限公司
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+.intel_syntax noprefix
+
+# gcc -nostdlib -static -z noexecstack hello.s
+
+# 查看退出状态码:
+# echo $?
+
+.section .text
+.globl _start
+_start:
+    mov rdi, 123 # bf 7b 00 00 00, 匹配 B8+rd id 规则. rdi 编号是 7, 所以 B8+7=BF
+    mov rax, 60  # b8 3c 00 00 00, 匹配 B8+rd id 规则. rax 编号是 0, 所以 B8+0=B8
+    syscall      # 0f 05, syscall 固定双字节操作码
+
