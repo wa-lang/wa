a028e8a7b6dcd88598dcf5b6d476ff808dc1dd0c
chai2010 <chaishushan@gmail.com>
2026-02-18 20:44:07 +0800

    native/link/pe: 补充代码注释

--- internal/native/link/pe/file.go
+++ internal/native/link/pe/file.go
@@ -5,6 +5,7 @@ package pe
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -20,16 +21,6 @@ type File struct {
 type Section struct {
 	SectionHeader64
 	sr *io.SectionReader
-}
-
-type ImportDirectory struct {
-	OriginalFirstThunk uint32
-	TimeDateStamp      uint32
-	ForwarderChain     uint32
-	Name               uint32
-	FirstThunk         uint32
-
-	dll string
 }
 
 func (s *Section) Data() ([]byte, error) {
@@ -71,50 +62,56 @@ func NewFilePE64(r io.ReaderAt) (*File, error) {
 	f := new(File)
 	sr := io.NewSectionReader(r, 0, 1<<63-1)
 
-	var dosheader [96]byte
-	if _, err := r.ReadAt(dosheader[0:], 0); err != nil {
+	// 读取 DOS 头部
+	// MS-DOS Stub 是可选的, 读取时忽略
+	var dosheader [KDosHeaderSize]byte
+	if n, err := r.ReadAt(dosheader[0:], 0); err != nil {
 		return nil, err
+	} else if n < KDosHeaderSize {
+		return nil, errors.New("invalid PE file")
 	}
-	var base int64
-	if dosheader[0] == 'M' && dosheader[1] == 'Z' {
-		signoff := int64(binary.LittleEndian.Uint32(dosheader[0x3c:]))
-		var sign [4]byte
-		r.ReadAt(sign[:], signoff)
-		if !(sign[0] == 'P' && sign[1] == 'E' && sign[2] == 0 && sign[3] == 0) {
-			return nil, fmt.Errorf("invalid PE file signature: % x", sign)
-		}
-		base = signoff + 4
-	} else {
-		base = int64(0)
+	if dosheader[0] != 'M' || dosheader[1] != 'Z' {
+		return nil, errors.New("invalid PE file")
 	}
-	sr.Seek(base, io.SeekStart)
+
+	// 从 DOS 头部获取 NtHeader64 的偏移地址
+	_NTHeaderOffset := int64(binary.LittleEndian.Uint32(dosheader[0x3c:]))
+
+	// 验证 PE 头部
+	var sign [4]byte
+	if n, err := r.ReadAt(sign[:], _NTHeaderOffset); err != nil {
+		return nil, fmt.Errorf("invalid PE file: %s", err)
+	} else if n < 4 {
+		return nil, fmt.Errorf("invalid PE file: read failed")
+	}
+	if sign[0] != 'P' && sign[1] != 'E' && sign[2] != 0 && sign[3] != 0 {
+		return nil, fmt.Errorf("invalid PE file signature: % x", sign)
+	}
+
+	// 读取 NtHeader64.FileHeader
+	sr.Seek(_NTHeaderOffset+4, io.SeekStart)
 	if err := binary.Read(sr, binary.LittleEndian, &f.FileHeader); err != nil {
 		return nil, err
 	}
-	switch f.FileHeader.Machine {
-	case IMAGE_FILE_MACHINE_AMD64,
-		IMAGE_FILE_MACHINE_I386,
-		IMAGE_FILE_MACHINE_UNKNOWN:
-		// ok
-	default:
+
+	// 判断 PE 程序对于的 CPU 类型
+	// 仅支持 amd64 和 386 两个类型
+	if m := f.FileHeader.Machine; m != IMAGE_FILE_MACHINE_AMD64 && m != IMAGE_FILE_MACHINE_I386 {
 		return nil, fmt.Errorf("unrecognized PE machine: %#x", f.FileHeader.Machine)
 	}
 
-	var err error
-
-	// Seek past file header.
-	_, err = sr.Seek(base+int64(binary.Size(f.FileHeader)), io.SeekStart)
+	// 读取 OptionalHeader64
+	_, err := sr.Seek(_NTHeaderOffset+4+int64(binary.Size(f.FileHeader)), io.SeekStart)
 	if err != nil {
 		return nil, err
 	}
-
-	// Read optional header.
 	f.OptionalHeader, err = readOptionalHeader(sr, f.FileHeader.SizeOfOptionalHeader)
 	if err != nil {
 		return nil, err
 	}
 
-	// Process sections.
+	// 读取全部的段头表
+	// 这里是真正的数据
 	f.Sections = make([]*Section, f.FileHeader.NumberOfSections)
 	for i := 0; i < int(f.FileHeader.NumberOfSections); i++ {
 		sh := new(SectionHeader64)
@@ -145,7 +142,6 @@ func NewFilePE64(r io.ReaderAt) (*File, error) {
 }
 
 func readOptionalHeader(r io.ReadSeeker, sz uint16) (*OptionalHeader64, error) {
-	// If optional header size is 0, return empty optional header.
 	if sz == 0 {
 		return nil, nil
 	}
@@ -170,121 +166,132 @@ func readOptionalHeader(r io.ReadSeeker, sz uint16) (*OptionalHeader64, error) {
 		return err == nil
 	}
 
+	// PE32+
 	if !read(&ohMagic) {
 		return nil, fmt.Errorf("failure to read optional header magic: %v", err)
-
 	}
-
-	switch ohMagic {
-	case PE64Magic: // PE32+
-		var (
-			oh64 OptionalHeader64
-			// There can be 0 or more data directories. So the minimum size of optional
-			// header is calculated by subtracting oh64.DataDirectory size from oh64 size.
-			oh64MinSz = binary.Size(oh64) - binary.Size(oh64.DataDirectory)
-		)
-
-		if sz < uint16(oh64MinSz) {
-			return nil, fmt.Errorf("optional header size(%d) is less minimum size (%d) for PE32+ optional header", sz, oh64MinSz)
-		}
-
-		// Init oh64 fields
-		oh64.Magic = ohMagic
-		if !read(&oh64.MajorLinkerVersion) ||
-			!read(&oh64.MinorLinkerVersion) ||
-			!read(&oh64.SizeOfCode) ||
-			!read(&oh64.SizeOfInitializedData) ||
-			!read(&oh64.SizeOfUninitializedData) ||
-			!read(&oh64.AddressOfEntryPoint) ||
-			!read(&oh64.BaseOfCode) ||
-			!read(&oh64.ImageBase) ||
-			!read(&oh64.SectionAlignment) ||
-			!read(&oh64.FileAlignment) ||
-			!read(&oh64.MajorOperatingSystemVersion) ||
-			!read(&oh64.MinorOperatingSystemVersion) ||
-			!read(&oh64.MajorImageVersion) ||
-			!read(&oh64.MinorImageVersion) ||
-			!read(&oh64.MajorSubsystemVersion) ||
-			!read(&oh64.MinorSubsystemVersion) ||
-			!read(&oh64.Win32VersionValue) ||
-			!read(&oh64.SizeOfImage) ||
-			!read(&oh64.SizeOfHeaders) ||
-			!read(&oh64.CheckSum) ||
-			!read(&oh64.Subsystem) ||
-			!read(&oh64.DllCharacteristics) ||
-			!read(&oh64.SizeOfStackReserve) ||
-			!read(&oh64.SizeOfStackCommit) ||
-			!read(&oh64.SizeOfHeapReserve) ||
-			!read(&oh64.SizeOfHeapCommit) ||
-			!read(&oh64.LoaderFlags) ||
-			!read(&oh64.NumberOfRvaAndSizes) {
-			return nil, fmt.Errorf("failure to read PE32+ optional header: %v", err)
-		}
-
-		dd, err := readDataDirectories(r, sz-uint16(oh64MinSz), oh64.NumberOfRvaAndSizes)
-		if err != nil {
-			return nil, err
-		}
-
-		copy(oh64.DataDirectory[:], dd)
-
-		return &oh64, nil
-	default:
+	if ohMagic != PE64Magic {
 		return nil, fmt.Errorf("optional header has unexpected Magic of 0x%x", ohMagic)
 	}
+
+	var (
+		oh64 OptionalHeader64
+		// There can be 0 or more data directories. So the minimum size of optional
+		// header is calculated by subtracting oh64.DataDirectory size from oh64 size.
+		oh64MinSz = binary.Size(oh64) - binary.Size(oh64.DataDirectory)
+	)
+
+	// 判断有问题, ohMagic 已经被读取过了
+	if sz < uint16(oh64MinSz) {
+		return nil, fmt.Errorf("optional header size(%d) is less minimum size (%d) for PE32+ optional header", sz, oh64MinSz)
+	}
+
+	// 读取 OptionalHeader64
+	// 不包含 Magic 和 DataDirectory 部分
+	oh64.Magic = ohMagic
+	if !read(&oh64.MajorLinkerVersion) ||
+		!read(&oh64.MinorLinkerVersion) ||
+		!read(&oh64.SizeOfCode) ||
+		!read(&oh64.SizeOfInitializedData) ||
+		!read(&oh64.SizeOfUninitializedData) ||
+		!read(&oh64.AddressOfEntryPoint) ||
+		!read(&oh64.BaseOfCode) ||
+		!read(&oh64.ImageBase) ||
+		!read(&oh64.SectionAlignment) ||
+		!read(&oh64.FileAlignment) ||
+		!read(&oh64.MajorOperatingSystemVersion) ||
+		!read(&oh64.MinorOperatingSystemVersion) ||
+		!read(&oh64.MajorImageVersion) ||
+		!read(&oh64.MinorImageVersion) ||
+		!read(&oh64.MajorSubsystemVersion) ||
+		!read(&oh64.MinorSubsystemVersion) ||
+		!read(&oh64.Win32VersionValue) ||
+		!read(&oh64.SizeOfImage) ||
+		!read(&oh64.SizeOfHeaders) ||
+		!read(&oh64.CheckSum) ||
+		!read(&oh64.Subsystem) ||
+		!read(&oh64.DllCharacteristics) ||
+		!read(&oh64.SizeOfStackReserve) ||
+		!read(&oh64.SizeOfStackCommit) ||
+		!read(&oh64.SizeOfHeapReserve) ||
+		!read(&oh64.SizeOfHeapCommit) ||
+		!read(&oh64.LoaderFlags) ||
+		!read(&oh64.NumberOfRvaAndSizes) {
+		return nil, fmt.Errorf("failure to read PE32+ optional header: %v", err)
+	}
+
+	// 读取 DataDirectory
+	dd, err := readDataDirectories(r, sz-uint16(oh64MinSz), oh64.NumberOfRvaAndSizes)
+	if err != nil {
+		return nil, err
+	}
+	copy(oh64.DataDirectory[:], dd)
+
+	return &oh64, nil
 }
 
+// 读取 DataDirectory
+func readDataDirectories(r io.ReadSeeker, sz uint16, n uint32) ([]DataDirectory, error) {
+	ddSz := uint64(binary.Size(DataDirectory{}))
+	if uint64(sz) != uint64(n)*ddSz {
+		return nil, fmt.Errorf("size of data directories(%d) is inconsistent with number of data directories(%d)", sz, n)
+	}
+
+	dd := make([]DataDirectory, n)
+	if err := binary.Read(r, binary.LittleEndian, dd); err != nil {
+		return nil, fmt.Errorf("failure to read data directories: %v", err)
+	}
+
+	return dd, nil
+}
+
+// 从 DataDirectory 读取导入符号信息
 func (f *File) ImportedSymbols() ([]string, error) {
 	if f.OptionalHeader == nil {
 		return nil, nil
 	}
 
-	// grab the number of data directory entries
-	var dd_length uint32
-	dd_length = f.OptionalHeader.NumberOfRvaAndSizes
-
-	// check that the length of data directory entries is large
-	// enough to include the imports directory.
-	if dd_length < IMAGE_DIRECTORY_ENTRY_IMPORT+1 {
+	// 检查 DataDirectory 数量
+	// 最后可能有一个空元素作为结束标志, 可能要多加一个
+	if f.OptionalHeader.NumberOfRvaAndSizes < IMAGE_DIRECTORY_ENTRY_IMPORT+1 {
 		return nil, nil
 	}
 
-	// grab the import data directory entry
-	var idd DataDirectory
-	idd = f.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT]
+	// 获取导入表对于的槽位
+	idd := f.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT]
 
-	// figure out which section contains the import directory table
+	// 查找导入表数据对应哪个段表格区间
 	var ds *Section
-	ds = nil
 	for _, s := range f.Sections {
 		if s.Offset == 0 {
 			continue
 		}
-		// We are using distance between s.VirtualAddress and idd.VirtualAddress
-		// to avoid potential overflow of uint32 caused by addition of s.VirtualSize
-		// to s.VirtualAddress.
+		// 以下的计算是为了避免 uint32 溢出, 用于判断 idd 是否在此段数据区间
+		// 问题: 并没有检查 idd.Size
 		if s.VirtualAddress <= idd.VirtualAddress && idd.VirtualAddress-s.VirtualAddress < s.VirtualSize {
 			ds = s
 			break
 		}
 	}
-
-	// didn't find a section, so no import libraries were found
 	if ds == nil {
 		return nil, nil
 	}
 
+	// 读取段数据
 	d, err := ds.Data()
 	if err != nil {
 		return nil, err
 	}
 
-	// seek to the virtual address specified in the import data directory
+	// 跳过段数据开头空白区域
 	d = d[idd.VirtualAddress-ds.VirtualAddress:]
 
-	// start decoding the import directory
+	// 每个 ImportDirectory 头部位 20字节
+	const kImportDirectoryMinSize = 20
+
+	// 从段数据解析导入符号信息
 	var ida []ImportDirectory
-	for len(d) >= 20 {
+	for len(d) >= kImportDirectoryMinSize {
 		var dt ImportDirectory
 		dt.OriginalFirstThunk = binary.LittleEndian.Uint32(d[0:4])
 		dt.TimeDateStamp = binary.LittleEndian.Uint32(d[4:8])
@@ -301,6 +308,7 @@ func (f *File) ImportedSymbols() ([]string, error) {
 	names, _ := ds.Data()
 	var all []string
 	for _, dt := range ida {
+		// 读取动态库的名字
 		dt.dll, _ = getString(names, int(dt.Name-ds.VirtualAddress))
 		d, _ = ds.Data()
 		// seek to OriginalFirstThunk
@@ -311,9 +319,11 @@ func (f *File) ImportedSymbols() ([]string, error) {
 			if va == 0 {
 				break
 			}
-			if va&0x8000000000000000 > 0 { // is Ordinal
-				// TODO: add dynimport ordinal support.
+			if va&0x8000000000000000 > 0 {
+				// TODO: 按序号导入函数
 			} else {
+				// 按照函数名导入
+				// 格式 ExitProcess:KERNEL32.dll
 				fn, _ := getString(names, int(uint32(va)-ds.VirtualAddress+2))
 				all = append(all, fn+":"+dt.dll)
 			}
@@ -323,20 +333,7 @@ func (f *File) ImportedSymbols() ([]string, error) {
 	return all, nil
 }
 
-func readDataDirectories(r io.ReadSeeker, sz uint16, n uint32) ([]DataDirectory, error) {
-	ddSz := uint64(binary.Size(DataDirectory{}))
-	if uint64(sz) != uint64(n)*ddSz {
-		return nil, fmt.Errorf("size of data directories(%d) is inconsistent with number of data directories(%d)", sz, n)
-	}
-
-	dd := make([]DataDirectory, n)
-	if err := binary.Read(r, binary.LittleEndian, dd); err != nil {
-		return nil, fmt.Errorf("failure to read data directories: %v", err)
-	}
-
-	return dd, nil
-}
-
+// 读取C风格的字符串, 以 \0 结尾
 func getString(section []byte, start int) (string, bool) {
 	if start < 0 || start >= len(section) {
 		return "", false
--- internal/native/link/pe/pe.go
+++ internal/native/link/pe/pe.go
@@ -13,6 +13,10 @@ const (
 	DefaultImageBase = 0x140000000
 )
 
+const (
+	KDosHeaderSize = 64
+)
+
 // DOS文件头部
 type DosHeader struct {
 	Magic          [2]byte // 'M', 'Z'
@@ -69,8 +73,8 @@ type OptionalHeader64 struct {
 	SizeOfHeapReserve           uint64
 	SizeOfHeapCommit            uint64
 	LoaderFlags                 uint32
-	NumberOfRvaAndSizes         uint32            // 无外部依赖的exe文件可以忽略
-	DataDirectory               [16]DataDirectory // 无外部依赖的exe文件可以忽略
+	NumberOfRvaAndSizes         uint32
+	DataDirectory               [16]DataDirectory // DLL 导入库在此定义
 }
 
 // 程序的元数据
@@ -92,6 +96,39 @@ type SectionHeader64 struct {
 	NumberOfRelocations  uint16  // 填 0
 	NumberOfLineNumbers  uint16  // 填 0
 	Characteristics      uint32  // 权限 (代码段: 0x60000020, 数据段: 0xC0000040)
+}
+
+// 导入符号字典
+type ImportDirectory struct {
+	// 指向 导入名称表的 RVA(相对虚拟地址)
+	// 指向一个数组, 数组里记录了要调用的所有函数的名字(比如 ExitProcess)
+	OriginalFirstThunk uint32
+
+	// 时间戳
+	// 如果该值为 0xFFFFFFFF, 表示该 DLL 已被绑定, 函数地址已提前写死在文件中.
+	// 但在现代开发和你的手搓编译器中, 通常直接填 0 即可
+	TimeDateStamp uint32
+
+	// 转发链 (通常被设置为 0)
+	// 用于处理 API 转发.
+	// 例如调用 DLL A 里的函数, 但 DLL A 实际上把这个请求转交给了 DLL B
+	// 这个字段在大多数情况下不需要手动处理
+	ForwarderChain uint32
+
+	// 指向 DLL 名称字符串 的 RVA
+	// 例如 "KERNEL32.dll"
+	Name uint32
+
+	// 指向 导入地址表 (Import Address Table, 简称 IAT) 的 RVA
+	// 和 OriginalFirstThunk 指向的内容几乎一样, 也是函数名
+	// 加载运行后, Windows 系统加载器会根据 OriginalFirstThunk 里的名字,
+	// 找到函数在内存里的真实地址，并覆盖写进 FirstThunk 指向的这个位置.
+	// 这是在汇编代码里 call [ExitProcess_IAT] 能跳到正确地方的原因.
+	FirstThunk uint32
+
+	// 非物理结构, 后来拼接
+	// ExitProcess:KERNEL32.dll
+	dll string
 }
 
 // 机器码类型
